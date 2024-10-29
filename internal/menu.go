32c3bc3bea55032cfcd868e65d452975dbcb3624
MarioRgzLpz <MarioRgzLpz@correo.ugr.es>
2024-10-29 13:08:19 +0100

    feat(M0): closes #10
    Creo un constructor que limita el número de platos ya que como mucho habrá 5 platos según lo reflejado en los ficheros de los que obtendremos los datos

--- internal/menu.go
+++ internal/menu.go
@@ -1,11 +1,23 @@
 package internal
 
 import (
+	"errors"
 	"time"
 )
 
 type Menu struct {
 	Nombre string
-	Fecha time.Time
+	Fecha  time.Time
 	Platos []Plato
+}
+
+func NuevoMenu(nombre string, fecha time.Time, platos []Plato) (*Menu, error) {
+	if len(platos) > 5 {
+		return nil, errors.New("El menú no puede tener más de 5 platos")
+	}
+	return &Menu{
+		Nombre: nombre,
+		Fecha:  fecha,
+		Platos: platos,
+	}, nil
 }
