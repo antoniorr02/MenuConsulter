f7c9fe1f938a5af6c0f5e5039d857585b34490f6
MarioRgzLpz <MarioRgzLpz@correo.ugr.es>
2024-10-31 08:37:27 +0100

    fix(M0): closes #10
    Elimino el atributo Nombre puesto que no es necesario según las HUs

--- internal/menu.go
+++ internal/menu.go
@@ -6,17 +6,15 @@ import (
 )
 
 type Menu struct {
-	Nombre string
 	Fecha  time.Time
 	Platos []Plato
 }
 
-func NuevoMenu(nombre string, fecha time.Time, platos []Plato) (*Menu, error) {
+func NuevoMenu(fecha time.Time, platos []Plato) (*Menu, error) {
 	if len(platos) > 5 {
 		return nil, errors.New("El menú no puede tener más de 5 platos")
 	}
 	return &Menu{
-		Nombre: nombre,
 		Fecha:  fecha,
 		Platos: platos,
 	}, nil
