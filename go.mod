60535232bd0ca390d3be68af29d4154b5c69d8e0
antoniorr <antoniorr@correo.ugr.es>
2024-11-24 12:27:26 +0100

    test para comprobar la extracción correcta de datos desde html, Closes #19

--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module MenuConsulter
 go 1.23.2
 
 require (
+	github.com/PuerkitoBio/goquery v1.10.0
+	golang.org/x/net v0.31.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
-	golang.org/x/net v0.31.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/extraccion_test.go
+++ internal/extraccion_test.go
@@ -48,3 +48,20 @@ func TestDownloadHTML(t *testing.T) {
 	//err = os.Remove(filepath)
 	//assert.NoError(t, err, "No se pudo eliminar el archivo de prueba")
 }
+
+func TestExtraerMenu(t *testing.T) {
+	filePath := "../data/menu.html"
+
+	menus, err := ExtraerMenus(filePath)
+
+	// Verificar que no hay errores en la extracción
+	assert.NoError(t, err, "La función ExtraerMenus devolvió un error")
+
+	// Comprobaciones sobre el contenido
+	assert.NotEmpty(t, menus, "No se extrajeron menús del archivo")
+
+	// Verificar algunos datos específicos de ejemplo
+	assert.Equal(t, "Menú 1", menus[0].Tipo, "El primer menú no tiene el tipo correcto")
+	assert.Contains(t, menus[0].Platos[0].Nombre, "Pastel de espinacas", "El nombre del primer plato no es correcto")
+	assert.Contains(t, menus[0].Platos[0].Ingredientes, "Gluten", "No se extrajo correctamente el alérgeno del primer plato")
+}
