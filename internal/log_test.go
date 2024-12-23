6b42dfe3a60842f9edf818828d03c90078577e81
antoniorr <antoniorr@correo.ugr.es>
2024-12-23 20:41:44 +0100

    Arreglo el test de #35

--- internal/log_test.go
+++ internal/log_test.go
@@ -1,22 +1,30 @@
 package internal
 
 import (
-	"bytes"
 	"log/slog"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoggerInitialization(t *testing.T) {
-	var buffer bytes.Buffer
+	logFile, err := os.CreateTemp("", "logfile-temporal.log")
+	assert.NoError(t, err, "No se pudo crear el archivo de log temporal")
+	defer os.Remove(logFile.Name())
 
-	handler := slog.NewTextHandler(&buffer, &slog.HandlerOptions{
+	handler := slog.NewTextHandler(logFile, &slog.HandlerOptions{
 		AddSource: false,
 	})
 
 	logger := slog.New(handler)
 
 	logger.Info("Esto es un mensaje de prueba")
-	if buffer.Len() == 0 {
-		t.Errorf("El logger no registró ningún mensaje")
-	}
+
+	logFile.Close()
+
+	content, err := os.ReadFile(logFile.Name())
+	assert.NoError(t, err, "No se pudo leer el archivo de log")
+
+	assert.Contains(t, string(content), "Esto es un mensaje de prueba", "El log debería contener el mensaje 'Esto es un mensaje de prueba'")
 }
