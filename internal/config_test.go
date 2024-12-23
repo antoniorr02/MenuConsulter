a434ccdee19f9175ed7328d37a459bc84aca011d
antoniorr <antoniorr@correo.ugr.es>
2024-12-23 20:26:04 +0100

    Arreglando el testing de #36

--- internal/config_test.go
+++ internal/config_test.go
@@ -1,27 +1,15 @@
 package internal
 
 import (
-	"bytes"
 	"testing"
 
 	"MenuConsulter/internal/config"
 
-	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInitLogger(t *testing.T) {
-	var logOutput bytes.Buffer
-
+func TestConfiguracion(t *testing.T) {
 	config.InitConfig()
 
-	logger := logrus.New()
-	logger.Out = &logOutput
-	logger.SetLevel(logrus.InfoLevel)
-	logger.SetFormatter(&logrus.TextFormatter{})
-
-	logger.Info("Prueba de log")
-
-	logContent := logOutput.String()
-	assert.Contains(t, logContent, "Prueba de log", "El contenido del log debería contener el mensaje esperado")
+	assert.Equal(t, "development", config.Config.GetString("app.env"), "El entorno por defecto debería ser 'development'")
 }
