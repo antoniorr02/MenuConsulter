502aa482b352b603b01aa1547a71dfe8d079e147
antoniorr <antoniorr@correo.ugr.es>
2024-12-27 18:55:07 +0100

    Poniendo urls en minúsculas en #39

--- internal/api_rest.go
+++ internal/api_rest.go
@@ -10,51 +10,51 @@ var router = chi.NewRouter()
 
 func Router() {
 	if router == nil {
-		router.Get("/Plato/{Nombre}", getPlato)
-		router.Put("/Plato/{Nombre}", putPlato)
-		router.Delete("/Plato/{Nombre}", deletePlato)
+		router.Get("/plato/{nombre}", getplato)
+		router.Put("/plato/{nombre}", putplato)
+		router.Delete("/plato/{nombre}", deleteplato)
 
-		router.Get("/Comedor/{Nombre}", getComedor)
-		router.Put("/Comedor/{Nombre}", putComedor)
-		router.Delete("/Comedor/{Nombre}", deleteComedor)
+		router.Get("/comedor/{nombre}", getcomedor)
+		router.Put("/comedor/{nombre}", putcomedor)
+		router.Delete("/comedor/{nombre}", deletecomedor)
 
-		router.Get("/Menu/{Fecha}", getMenu)
-		router.Put("/Menu/{Fecha}", putMenu)
-		router.Delete("/Menu/{Fecha}", deleteMenu)
+		router.Get("/menu/{fecha}", getmenu)
+		router.Put("/menu/{fecha}", putmenu)
+		router.Delete("/menu/{fecha}", deletemenu)
 	}
 }
 
-func getPlato(w http.ResponseWriter, r *http.Request) {
+func getplato(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func putPlato(w http.ResponseWriter, r *http.Request) {
+func putplato(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func deletePlato(w http.ResponseWriter, r *http.Request) {
+func deleteplato(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func getComedor(w http.ResponseWriter, r *http.Request) {
+func getcomedor(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func putComedor(w http.ResponseWriter, r *http.Request) {
+func putcomedor(w http.ResponseWriter, r *http.Request) {
 }
 
-func deleteComedor(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func getMenu(w http.ResponseWriter, r *http.Request) {
+func deletecomedor(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func putMenu(w http.ResponseWriter, r *http.Request) {
+func getmenu(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func deleteMenu(w http.ResponseWriter, r *http.Request) {
+func putmenu(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deletemenu(w http.ResponseWriter, r *http.Request) {
 
 }
