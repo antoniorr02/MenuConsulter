4a7c9db7ef945ebe01fe01f3850db96a647ee0f6
antoniorr <antoniorr@correo.ugr.es>
2024-12-31 10:48:49 +0100

    Comenzando con #45

--- internal/api_rest.go
+++ internal/api_rest.go
@@ -13,17 +13,14 @@ func Router() {
 		router.Route("/comedores", func(r chi.Router) {
 			r.Get("/{nombre_comedor}", getcomedor)
 			r.Put("/{nombre_comedor}", putcomedor)
-			r.Delete("/{nombre_comedor}", deletecomedor)
 
 			r.Route("/{nombre_comedor}/menus", func(r chi.Router) {
 				r.Get("/{fecha}", getmenu)
 				r.Put("/{fecha}", putmenu)
-				r.Delete("/{fecha}", deletemenu)
 
 				r.Route("/{fecha}/platos", func(r chi.Router) {
 					r.Get("/{nombre_plato}", getplato)
 					r.Put("/{nombre_plato}", putplato)
-					r.Delete("/{nombre_plato}", deleteplato)
 				})
 			})
 		})
@@ -38,10 +35,6 @@ func putplato(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func deleteplato(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func getcomedor(w http.ResponseWriter, r *http.Request) {
 
 }
@@ -49,18 +42,10 @@ func getcomedor(w http.ResponseWriter, r *http.Request) {
 func putcomedor(w http.ResponseWriter, r *http.Request) {
 }
 
-func deletecomedor(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func getmenu(w http.ResponseWriter, r *http.Request) {
 
 }
 
 func putmenu(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func deletemenu(w http.ResponseWriter, r *http.Request) {
 
 }
