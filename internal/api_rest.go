d20a247aac8dd2aa0e6518f03a0f868a0af836c3
antoniorr <antoniorr@correo.ugr.es>
2025-01-02 10:02:10 +0100

    Elimino la operación PUT del API, #45

--- internal/api_rest.go
+++ internal/api_rest.go
@@ -16,28 +16,21 @@ func Router() {
 
 func comedoresRoutes(comedorRouter chi.Router) {
 	comedorRouter.Get("/{nombre_comedor}", getcomedor)
-	comedorRouter.Put("/{nombre_comedor}", putcomedor)
 
 	comedorRouter.Route("/{nombre_comedor}/menus", menusRoutes)
 }
 
 func menusRoutes(menuRouter chi.Router) {
 	menuRouter.Get("/{fecha}", getmenu)
-	menuRouter.Put("/{fecha}", putmenu)
 
 	menuRouter.Route("/{fecha}/platos", platosRoutes)
 }
 
 func platosRoutes(platoRouter chi.Router) {
 	platoRouter.Get("/{nombre_plato}", getplato)
-	platoRouter.Put("/{nombre_plato}", putplato)
 }
 
 func getplato(respuesta http.ResponseWriter, peticion *http.Request) {
-
-}
-
-func putplato(respuesta http.ResponseWriter, peticion *http.Request) {
 
 }
 
@@ -45,12 +38,6 @@ func getcomedor(respuesta http.ResponseWriter, peticion *http.Request) {
 
 }
 
-func putcomedor(respuesta http.ResponseWriter, peticion *http.Request) {
-}
-
 func getmenu(respuesta http.ResponseWriter, peticion *http.Request) {
 
-}
-
-func putmenu(respuesta http.ResponseWriter, peticion *http.Request) {
 }
